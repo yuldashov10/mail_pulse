a3e85dc85a041def63fb8af9a6b6481043856b47
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-03-21 10:27:36 +0300

    users | Админка
    - Модель `User` зарегистрирована в панели администратора Django.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Админка для пользователей."""
+
+    list_display = ("email", "username", "country", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "country")
+    search_fields = ("email", "username")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Персональная информация",
+            {"fields": ("username", "avatar", "phone_number", "country")},
+        ),
+        (
+            "Права доступа",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Даты", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+    ordering = ("email",)
